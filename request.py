5848c14535cb65b2f64208247e071466e8378058
Jim Wei <114387166+GuilhermeJim@users.noreply.github.com>
2022-10-30 08:07:25 -0300

    Add files via upload

---
+++ request.py
@@ -0,0 +1,11 @@
+# https://www.fasdapsicanalise.com.br/este-teste-de-4-perguntas-diz-perfeitamente-a-sua-personalidade/
+
+from Test.questions import *
+
+#Instruções do teste
+instructions()
+
+#Questões
+results()
+
+
